1b9694c9d401ea05b4fbb38702dbf4b6ae27f839
ktiempo <tiempo.kryselapril@gmail.com>
2025-10-12 02:23:56 +0800

    landing to login

---
+++ view/admin/index.php
@@ -0,0 +1,14 @@
+<?php
+include("../../auth/authentication.php");
+include("./includes/header.php");
+include("./includes/topbar.php");
+include("./includes/sidebar.php");
+?>
+
+
+
+
+
+<?php
+include("./includes/footer.php");
+?>
